13330a79cd1668bcc2790156ad9ab340948028a3
Gabriel <gabriel@gabriel-desktop.(none)>
2013-04-29 16:36:30 +0300

    exercise2

--- 07_Javascript_basics/exercise.js
+++ 07_Javascript_basics/exercise.js
@@ -1,11 +1,32 @@
-/*Confirm
+/*Old enough to play?
 
-You have some programming skills. Time to make something you can show people! We're going to show you how to program a 'choose your own adventure' game. It'll have a basic story line, have the user make some choices, and have a happy ending. Then you can modify it as you wish and show off your creative talents!
+With great programming powers, comes great responsibility. We should check if the user is old enough to play!
+
+This involves two bits of code.
+
+    Using prompt to ask the user for their age. You can do it like this: var age = prompt("What's your age?"); and the variable age will hold the user's response.
+    Writing an if / else statement based on the age of the user.
+
+Note: we have to store the result of the prompt in a variable! Otherwise, we can't make use of it in the if / else statement.
 Instructions
 
-    It's always polite to ask your user if they are ready to play.
-    Using the confirm command, make sure your user is ready to play. For example, I would use the sentence "I am ready to play!". Add whatever phrase you would like in the confirm.
+    Under the existing code, declare a variable age.
+    Make age equal to the answer you prompt the user to give (ie. ask the user 'how old are you?')
+    If the user is less than 18, use console.log and tell them they're allowed to play but you take no responsibility.
+    Else, use console.log and give them an encouraging message to play on!
+
 
 */
 // Check if the user is ready to play!
-confirm("I am ready to play!")
+confirm("I am ready to play!");
+
+var age =prompt("What's your age");
+
+if(age<18)
+{
+    console.log("You're allowed to play but we are not responsible for you")
+}
+else
+{
+    console.log("Great! let's play")
+}
