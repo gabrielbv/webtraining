d4c2832e5c66f44f6ef33cfd71fc54b9a59e8434
Gabriel <gabriel@gabriel-desktop.(none)>
2013-04-29 16:29:30 +0300

    exercise1

---
+++ 07_Javascript_basics/exercise.js
@@ -0,0 +1,11 @@
+/*Confirm
+
+You have some programming skills. Time to make something you can show people! We're going to show you how to program a 'choose your own adventure' game. It'll have a basic story line, have the user make some choices, and have a happy ending. Then you can modify it as you wish and show off your creative talents!
+Instructions
+
+    It's always polite to ask your user if they are ready to play.
+    Using the confirm command, make sure your user is ready to play. For example, I would use the sentence "I am ready to play!". Add whatever phrase you would like in the confirm.
+
+*/
+// Check if the user is ready to play!
+confirm("I am ready to play!")
