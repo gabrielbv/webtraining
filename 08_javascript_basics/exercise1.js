a34f311ef4f67aaec36136ad2bfc69520977e766
Gabriel <gabriel@gabriel-desktop.(none)>
2013-05-07 09:56:08 +0300

    exercise1

---
+++ 08_javascript_basics/exercise1.js
@@ -0,0 +1,10 @@
+/*User Choice
+
+We start by first asking the user which option they want to pick. We will later use this choice in the compare function to determine the winner.
+Instructions
+
+    Declare a variable called userChoice.
+    Make the variable equal to the answer we get by asking the user "Do you choose rock, paper or scissors?"
+
+*/
+var userChoice= prompt("Do you choose rock,paper or scissors?")
