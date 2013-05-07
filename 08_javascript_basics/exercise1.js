1f6e18ce0a172deed272cd3bbbe820dbb2f72dca
Gabriel <gabriel@gabriel-desktop.(none)>
2013-05-07 10:09:04 +0300

    exercise4

--- 08_javascript_basics/exercise1.js
+++ 08_javascript_basics/exercise1.js
@@ -1,32 +1,30 @@
 /*
-Computer Choice: Part 2
+Both choices are the same!
 
-We have computerChoice but it now equals a random number between 0 and 1. We need to somehow translate this random number into a random choice of rock, paper, or scissors. How do we do this?!
+Now comes the fun part! We need to create a function. It will take two parameters (ie. the two choices made) and then return the winning choice.
 
-    If computerChoice is between 0 and 0.33, make computerChoice equal to rock.
-    If computerChoice is between 0.34 and 0.66, make computerChoice equal to paper.
-    If computerChoice is between 0.67 and 1, make computerChoice equal to scissors.
-
-But there are three outcomes! If / else only lets us have two outcomes. What now?! We need to use if / else if / else. See the hint for the full sytnax. You will laugh at how easy it is.
+When programming a game like this, you have to first figure out all the various outcomes. One outcome is that the choice the user makes is equal to the choice the computer makes.
 Instructions
 
-    Under your existing code, write out the if / else if / else statement.
-    In the respective code blocks, change the value of computerChoice based on the rules stated above. Remember, you do NOT have to use var when changing the value of a variable that already exists.
+    We carried over the code from the previous section, but it is a comment. Leave it there for now.
+    Below the comment, declare a function called compare().
+    It takes two parameters, choice1 and choice2. There is a reason we don't use the variable names (userChoice/computerChoice) as the parameters! (Read the Hint for more.)
+    Inside the function, write an if statement such that if the two parameters equal each other, the function will return "The result is a tie!"
 
 
 
 */
-var userChoice= prompt("Do you choose rock,paper or scissors?")
-var computerChoice=Math.random()
-console.log(computerChoice)
-if (computerChoice<0.33)
-{
-    console.log("rock")
+/*var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+}*/
+var compare = function(choice1,choice2){
+if(choice1===choice2){
+    return "The result is a tie!"
 }
-else if(computerChoice>0.67)
-{
-    console.log("scissors")
-}
-else{
-    console.log("paper")
 }
