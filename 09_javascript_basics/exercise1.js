e883a26bf5189727dd80d43d3d9bebc72116179b
Gabriel <gabriel@gabriel-desktop.(none)>
2013-05-07 12:55:27 +0300

    exercise1

---
+++ 09_javascript_basics/exercise1.js
@@ -0,0 +1,25 @@
+/*
+
+Declare your variables
+
+First, we'll start by declaring the variables we'll be using: text, myName, and hits.
+
+Since text could be quite long, try putting a backslash (\) at the end of each line to make your string "wrap" to the next line of the editor. That way you avoid really long lines! (Feel free to peek back at the first exercise for an example.)
+
+You can ignore the /*jshint... line for now. All that does is tell the console to stop worrying about our use of backslash characters for wrapping long lines of text.
+Instructions
+
+Declare and set the following three variables:
+
+    text, and make it a string containing some text. Place your name in there a couple of times!
+    myName, and make it a string containing just your name.
+    hits, and make it an empty array.
+
+
+*/
+
+/*jshint multistr:true */
+
+var text ="Gabi Gabi Gabi Gabi Gabi Gabi Gabi Gabi Gabi Gabi Gabi Gabi"
+var myName="Gabi"
+var hits=[]
