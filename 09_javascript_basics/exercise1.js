354289931cc5fcbaca9369bb62e1167c6b249053
Gabriel <gabriel@gabriel-desktop.(none)>
2013-05-07 13:00:32 +0300

    exercise3

--- 09_javascript_basics/exercise1.js
+++ 09_javascript_basics/exercise1.js
@@ -1,13 +1,27 @@
 /*
 
-Your first "for" loop
+Your "if" statement
 
-Awesome! Now let's write our outer for loop.
+Nice work! Now let's move on to the if statement.
+
+We'll want to place the if statement inside our for loop to make sure the program checks the if statement each time it moves forward through the loop. Essentially, the for loop is saying: "Hey program! Go through every letter in 'text'." The if statement will say: if you see something interesting, push that text into an array!"
+
+You can treat a string like an array of characters. For instance, you know that
+
+var myArray = ['hello', 'world'];
+myArray[0]; // equals 'hello'
+
+But this also works on strings!
+
+var myName = 'Eric';
+myName[0] // equals 'E'
+
+The new
 Instructions
 
-Below your existing code, create a for loop that starts at 0, continues until it reaches the end of text, and increments by 1 each time. (This means it will check each character in the string.) There's no need to write anything between the {}s of your loop just yet.
+Add your if statement in the body of your for loop. It should check to see whether the current letter is equal to the first letter of your name. (Capitalization counts!)
 
-Check the Hint if you need help!
+There's no need to put anything between the {}s of your if just yet.
 
 
 */
@@ -18,4 +32,6 @@ var text ="Gabi Gabi Gabi Gabi Gabi Gabi Gabi Gabi Gabi Gabi Gabi Gabi"
 var myName="Gabi"
 var hits=[]
 
-for(i=0; i>text.length ; i++){}
+for(i=0; i>text.length ; i++){
+    if(text[i]==='G'){}
+}
