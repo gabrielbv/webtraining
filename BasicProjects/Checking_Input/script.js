97affb55d55a268974b4ded772db193866450f17
gabi <gabi@gabi-F5SL.(none)>
2013-05-16 15:36:07 +0300

    exercise1

---
+++ BasicProjects/Checking_Input/script.js
@@ -0,0 +1,11 @@
+/*
+Declare Your Variable
+
+First things first: let's declare the userAge variable.
+Instructions
+
+Go ahead and create the variable userAge in the editor.
+*/
+
+// Write your code below!
+var userAge;
