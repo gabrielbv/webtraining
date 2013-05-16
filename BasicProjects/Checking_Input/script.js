574bac8320f065b84e5e5f73f8d3b88aa91113aa
gabi <gabi@gabi-F5SL.(none)>
2013-05-16 15:48:38 +0300

    exercise3

--- BasicProjects/Checking_Input/script.js
+++ BasicProjects/Checking_Input/script.js
@@ -1,25 +1,22 @@
 /*
-Prompting the User
+isNaN()
 
-Good! Now we'll want to do two things:
+Great work! If you tried a few non-numeric inputs, though, you probably noticed what JavaScript returned: NaN.
 
-    Prompt the user for input, and
-    Try to convert that input into a number.
+NaN stands for "not a number" and is what JavaScript spits out when you give a non-number to parseInt(). As responsible JavaScript programmers, we should catch when a user enters a non-number and log an error message to the console.
 
-JavaScript can get input from the user using the prompt() function. You can type a message in quotes between the parentheses to ask the user for input, like so:
-
-prompt("What's up?");
-
-This function returns a string, so rather than setting userAge equal to the prompt, we want to put another function around the prompt that will turn the value prompt() gets from the user into a number. We can do that with the parseInt() function, which works like this:
-
-parseInt("4", 10);
-
-where "4" is the thing we want to change from a string into a number, and 10 means "make this a base-10 number." (If you don't quite know what this means, don't worry—you just need to know that regular, run-of-the-mill numbers are always base-10.)
+JavaScript has a built-in function, isNaN(), that returns true when you give it a value that's NaN and false otherwise. We'll use this to console.log() a message if the userAge ends up not being a number.
 Instructions
 
-Set your userAge variable equal to the result of passing prompt() to parseInt(). (That is, prompt() should go between parseInt()'s parentheses.)
+Set up an if statement in the editor. It should check if userAge isNaN(); if so, it should log a message to the console. (No need for an else statement just yet.)
+
+Go ahead and provide a non-number as input in order to trigger your console.log()!
 
 */
 
 // Write your code below!
 var userAge=parseInt(prompt('What is your age?'),10);
+
+if(isNaN(userAge)){
+    console.log("Please enter a number")
+}
