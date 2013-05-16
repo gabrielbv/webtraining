919af01ab05ce6d1519bbc9b32388afc367de85b
gabi <gabi@gabi-F5SL.(none)>
2013-05-16 16:49:07 +0300

    exercise4

--- BasicProjects/Draw_JavaScript/script.js
+++ BasicProjects/Draw_JavaScript/script.js
@@ -1,20 +1,17 @@
 /*
-Drawing a Circle
 
-We saw how to draw a semicircle in the last exercise. By going all the way around to 2π, we can draw an entire circle!
+Drawing a Rectangle
 
-The code looks something like this:
+Perfect! See how easy this is?
 
-context.arc(95, 85, 40, 0, 2*Math.PI);
+You can draw squares and rectangles using the .strokeRect() method, which takes four arguments: how far in from the left to put the upper-left corner of the rectangle, how for down to put the upper-left corner of the rectangle, the width of the rectangle, and the height. An example might be:
 
-Remember, the order of arguments is:
+context.strokeRect(10, 10, 50, 20);
 
-.arc(x, y, radius, startAngle, endAngle)
-
-where x is how far over to the right you start, y is how far down you start, radius is the radius of the circle, startAngle is the beginning angle, and endAngle is the ending angle. A circle goes from the angles 0 to 2 * Math.PI.
+You don't need to define any paths for rectangles; the above line of code alone will do the trick!
 Instructions
 
-Update your .arc() to draw a circle in the editor. It can be any size you want!
+Go ahead and draw your own rectangle.
 
 */
 var my_canvas=document.getElementById("canvas");
@@ -23,3 +20,4 @@ var context = my_canvas.getContext("2d");
 context.beginPath();
 context.arc(80,75,40,0,2*Math.PI);
 context.stroke();
+context.strokeRect(10, 10, 50, 20);
